5838b3fba765fb2f61d8541a5ed62269ffffbfab
Avinashpalofficial <apal73451@gmail.com>
2026-02-06 15:22:39 +0530

    logout_User

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -49,3 +49,16 @@ export  const loginUser = catchAsyncError(async(req,res,next)=>{
            sendToken(user,200,res)
 
 })
+export const logoutUser = catchAsyncError(async(req,res,next)=>{
+                res.cookie('token',null,{
+                      expires:new Date(0),
+                      httpOnly:true,
+                      secure:process.env.NODE_ENV === 'PRODUCTION',
+                      sameSite:"strict"
+                })  
+                res.status(200).json({
+                        success:true,
+                        message:"logout successfully"
+                })
+
+})
